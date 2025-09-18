3c4769e54f530cd70249626b889de9e25058f2da
J2ANS0l0 <J2AN.S0l0@gmail.com>
2025-09-17 19:57:46 -0600

    Complete schema.sql

--- schema.sql
+++ schema.sql
@@ -1,56 +1,57 @@
 
+DROP TABLE IF EXISTS card_in_collection;
+DROP TABLE IF EXISTS collection;
+DROP TABLE IF EXISTS price;
+DROP TABLE IF EXISTS card_condition;
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS "user";
+
 CREATE TABLE "user" (
-  "user_id" SERIAL PRIMARY KEY,
+  "user_id" integer PRIMARY KEY,
   "name" varchar,
   "email" varchar UNIQUE,
-  "created_at" timestamp DEFAULT now()
+  "created_at" timestamp
 );
 
 CREATE TABLE "card" (
-  "card_id" SERIAL PRIMARY KEY,
+  "card_id" integer PRIMARY KEY,
   "name" varchar,
   "language" varchar,
   "set_name" varchar,
   "set_number" varchar,
   "finish_type" varchar,
-  "image_url" varchar
+  "image_url" varchar,
+  "edition" varchar
 );
 
 CREATE TABLE "card_condition" (
-  "condition_id" SERIAL PRIMARY KEY,
+  "condition_id" integer PRIMARY KEY,
   "description" varchar
 );
 
 CREATE TABLE "price" (
-  "card_id" integer NOT NULL,
-  "condition_id" integer NOT NULL,
-  "price_usd" decimal,
-  "created_at" timestamp DEFAULT now(),
-  PRIMARY KEY ("card_id", "condition_id", "created_at"),
-  FOREIGN KEY ("card_id") REFERENCES "card" ("card_id"),
-  FOREIGN KEY ("condition_id") REFERENCES "card_condition" ("condition_id")
+  "card_id" integer,
+  "condition_id" integer,
+  "price_usd" decimal NOT NULL,
+  "date" timestamp,
+  PRIMARY KEY ("card_id", "condition_id")
 );
 
 CREATE TABLE "collection" (
-  "collection_id" SERIAL PRIMARY KEY,
+  "collection_id" integer PRIMARY KEY,
   "title" varchar,
   "user_id" integer NOT NULL,
   "exchange_rate" decimal,
   "collection_price_usd" decimal,
-  "created_at" timestamp DEFAULT now(),
-  FOREIGN KEY ("user_id") REFERENCES "user" ("user_id")
+  "created_at" timestamp
 );
 
 CREATE TABLE "card_in_collection" (
-  "card_collection_id" SERIAL PRIMARY KEY,
+  "card_collection_id" integer PRIMARY KEY,
   "collection_id" integer NOT NULL,
   "card_id" integer NOT NULL,
   "condition_id" integer NOT NULL,
-  "quantity" integer,
-  "price" decimal,
-  FOREIGN KEY ("collection_id") REFERENCES "collection" ("collection_id"),
-  FOREIGN KEY ("card_id") REFERENCES "card" ("card_id"),
-  FOREIGN KEY ("condition_id") REFERENCES "card_condition" ("condition_id")
+  "quantity" integer
 );
 
 COMMENT ON COLUMN "card"."language" IS 'EN or JP';
@@ -61,3 +62,10 @@ COMMENT ON COLUMN "card"."image_url" IS 'link to official image';
 COMMENT ON COLUMN "card_condition"."description" IS 'Example: Near Mint, Lightly Played';
 COMMENT ON COLUMN "collection"."exchange_rate" IS 'Negotiable per collection';
 COMMENT ON COLUMN "collection"."collection_price_usd" IS 'CALCULATED VALUE FROM SUM OF INDIVIDUAL CARDS';
+
+ALTER TABLE "collection" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+ALTER TABLE "card_in_collection" ADD FOREIGN KEY ("collection_id") REFERENCES "collection" ("collection_id");
+ALTER TABLE "price" ADD FOREIGN KEY ("card_id") REFERENCES "card" ("card_id");
+ALTER TABLE "price" ADD FOREIGN KEY ("condition_id") REFERENCES "card_condition" ("condition_id");
+
+ALTER TABLE "card_in_collection" ADD FOREIGN KEY ("card_id", "condition_id") REFERENCES "price" ("card_id", "condition_id");
