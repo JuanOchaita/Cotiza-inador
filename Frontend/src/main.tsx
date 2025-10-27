064d867e543827e678d27d6b365e87130e709181
PhilipFalla <philipfalla@ufm.edu>
2025-10-27 17:13:05 -0400

    Frontend

---
+++ Frontend/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import App from "./App.tsx";
+import "./index.css";
+import { LanguageProvider } from "./contexts/LanguageContext";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
